dc3808f08aad37bd07a2d668f26df9d6cb068a57
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2022-03-17 19:43:34 -0300

    update 1

--- perceptron.js
+++ perceptron.js
@@ -13,35 +13,40 @@ const WEIGHT = [0, 0, 0];
 let functionYent = [];
 
 let alpha = 1;
-const threshold = 2;
+const threshold = 0.2;
 
-const hasWeightVariation = true;
+let hasWeightVariation = true;
+
+let teste = 0;
 
 function handle() {
-  // while (hasWeightVariation) {
+  while (hasWeightVariation) {
     
     for (let line = 0; line < 4; line++) {
       let yent = 0;
 
       for (let column = 0; column < 3; column++) {
-        // console.log(TRUTH_TABLE[line][column] * WEIGHT[column])
-        // console.log(WEIGHT[column])
         yent += parseInt(TRUTH_TABLE[line][column] * WEIGHT[column]);
       }
+
       yent += + TRUTH_TABLE[line][2];
-      console.log(yent)
 
-      // functionYent.push(yent);
+      functionYent.push(yent);
 
-      // if (functionYent[line] === TARGET[line]) {
-      //   hasWeightVariation = false;
-      //   break;
-      // }
+      if (functionYent[line] === TARGET[line]) {
+        hasWeightVariation = false;
+        break;
+      }
 
-      // WEIGHT[line] = alpha * (TARGET[line] - functionYent[line]) * 
+      WEIGHT[0] = alpha * (TARGET[line] - functionYent[line]) * TRUTH_TABLE[line][0];
+      WEIGHT[1] = alpha * (TARGET[line] - functionYent[line]) * TRUTH_TABLE[line][1];
+      WEIGHT[2] = alpha * (TARGET[line] - functionYent[line]) * TRUTH_TABLE[line][2];
     }
   
-  // }
+    teste += 1;
+  }
+
+  console.log('gera -> ', teste);
 }
 
 handle();
