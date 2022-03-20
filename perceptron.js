4f9bf337268eb32562262f40a7882c7d9de57b04
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2022-03-20 10:28:15 -0300

    Update: refatoração para receber AND, OR e XOR

--- perceptron.js
+++ perceptron.js
@@ -1,82 +1,83 @@
-// AND - Truth Table
-const TRUTH_TABLE = [
-  [1, 1, 1],
-  [1, -1, 1],
-  [-1, 1, 1],
-  [-1, -1, 1],
-];
-
-const TARGET = [1, -1, -1, -1];
-
-let WEIGHT = [0, 0, 0];
-
-let functionYent;
+const DATA = require("./data_input.json");
 
 const alpha = 1;
 const threshold = 0.2;
 
-let hasWeightVariation = true;
+function handle(operation) {
+  // Declaração e inicialização de variáveis
+  let fYent = null;
+  let generation = 0;
+  let WEIGHT = [0, 0, 0];
+  let hasWeightVariation = true;
+  let weightVariation = [[], [], [], []];
+  
+  // Captura da tabela verdade e alvos de acordo com a operação (AND, OR ou XOR)
+  const { TRUTH_TABLE, TARGET } = DATA[operation];
 
-let weightVariation = [[], [], [], []];
-
-let teste = 0;
-
-function handle() {
   while (hasWeightVariation) {
-
     for (let line = 0; line < 4; line++) {
       let yent = 0;
 
+      // Cálculo do Yent
       for (let column = 0; column < 3; column++) {
         yent += parseInt(TRUTH_TABLE[line][column] * WEIGHT[column]);
       }
 
+      // Captura do (f)Yent
+      fYent = getFYent(yent);
 
-      console.log('yent ->', yent);
+      let wVariationFirstPart = parseInt(alpha * (TARGET[line] - fYent));
 
-      if (yent > threshold) functionYent = 1;
-      else if (yent <= threshold && yent >= threshold * -1) functionYent = 0;
-      else functionYent = -1;
-
-      console.log('fyent -> ', functionYent);
-
-      let wVariationFirstPart = parseInt(alpha * (TARGET[line] - functionYent));
-
+      // Mapeamento das variações dos pesos (matriz(3,4))
       weightVariation[line][0] = parseInt(wVariationFirstPart * TRUTH_TABLE[line][0]);
       weightVariation[line][1] = parseInt(wVariationFirstPart * TRUTH_TABLE[line][1]);
       weightVariation[line][2] = parseInt(wVariationFirstPart * TRUTH_TABLE[line][2]);
 
+      // Atualização dos valores dos pesos
       WEIGHT[0] += parseInt(weightVariation[line][0]);
       WEIGHT[1] += parseInt(weightVariation[line][1]);
       WEIGHT[2] += parseInt(weightVariation[line][2]);  
-
     }
-    console.log('-----------Fim da época-----------')
+    
+    const done = noWeightVariation(weightVariation);
 
-    let done = true;
+    generation += 1;
 
-    for (let i = 0; i < 4; i++) {
-      for (let j = 0; j < 3; j++) {
-        console.log('variacao -> ', weightVariation[i][j]);
-        if (weightVariation[i][j] !== 0) {
-          done = false;
-          break;
-        }
-      }
-      if (!done) break;
-    }
-
-
-    if (!done) {
-      teste += 1;
-    }
-    else {
-      teste += 1;
+    if (done) {
       hasWeightVariation = false;
+      continue;
     }
   }
   
-  console.log('gera -> ', teste);
+  console.log(`Operação ${operation} => ${generation} gerações.`);
 }
 
-handle();
+function getFYent(yent) {
+  if (yent > threshold) {
+    return 1;
+  } else if (yent <= threshold && yent >= -threshold) {
+    return 0;
+  } else {
+    return -1;
+  }
+}
+
+function noWeightVariation(weightVariation) {
+  let done = true;
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (weightVariation[i][j] !== 0) {
+        done = false;
+        break;
+      }
+    }
+
+    if (!done) break;
+  }
+
+  return done;
+}
+
+handle("AND");
+// handle("OR");
+// handle("XOR");
